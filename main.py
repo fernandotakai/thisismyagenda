dcb7bc490b4779efb7f5d294a5bb17f00bb4db6b
Fernando Takai <fernando.takai@gmail.com>
2010-06-30 21:00:22 -0300

    * Implemented a simple command parser for xmpp commands * added /list, /delete, /verify and /help commands

--- main.py
+++ main.py
@@ -27,6 +27,13 @@ Best regards,
 This is your agenda.
 """
 
+XMPP_HELP = """this is my help.
+/list - list all your tasks
+/verify [api] - verifies this account for xmpp commands
+/delete [key] - deletes a task
+/help - shows this help
+"""
+
 class BaseHandler(tornado.web.RequestHandler):
     """Implements Google Accounts authentication methods."""
     def get_current_user(self):
@@ -163,16 +170,44 @@ class DatePreviewHandler(BaseHandler):
 
 class XMPPHandler(tornado.web.RequestHandler):
 
-    def list_tasks(self, email):
+    def help(self, to):
+        xmpp.send_message(to, XMPP_HELP)
+
+    def list(self, email, when=None):
         tasks = ""
 
         for task in models.Task.tasks_by_user(email):
-            tasks += "* Task: %s due on %s\n" % (task.description, task.due_on.strftime("%F - %T"))
+            tasks += "* id %s: %s due on %s\n" % (task.key(), task.description, task.due_on.strftime("%F - %T"))
 
         if len(tasks) == 0:
             tasks = "You have no tasks. Go procrastinate!"
 
         xmpp.send_message(email, tasks)
+
+    def delete(self, email, key):
+        task = Task.get(key)
+        
+        if not task or task.user.email() != email:
+            xmpp.send_message(email, "Sorry, could not find that task… try a /list first")
+            return
+
+        task.delete()
+
+        xmpp.send_message(email, "Task deleted!")
+
+    def verify(self, email, api_key):
+        settings = models.UserSettings.get_or_create(email)
+        message = "Sorry, could not validate your key… Go to http://thisismyagenda.appspot.com/settings and make sure it's correct"
+
+        if settings.api_key == api_key:
+            settings.verified = True
+            message = "Hurray! Validated!"
+
+        xmpp.send_message(email, message)
+
+    def validate_user(self, email):
+        settings = models.UserSettings.get_or_create(email)
+        return settings.verified
 
     def post(self):
         _from = self.get_argument("from")
@@ -182,8 +217,39 @@ class XMPPHandler(tornado.web.RequestHandler):
         if "/" in _from:
             _from = _from.split("/", 1)[0]
 
-        if body.startswith("/list"):
-            self.list_tasks(_from)
+        if not self.validate_user(_from):
+            xmpp.send_message(_from, """Sorry, you didn't validated your email. 
+            Can you go to http://thisismyagenda.appspot.com/settings, get your api key
+            and issue a /verify api-key, so you can use me? thx.""")
+            return
+
+        try:
+            body += " "
+            command, value = body.split(" ", 1)
+        except Exception, e:
+            logging.exception("error while parsing xmpp body from %s" % _from)
+            self.help(_from)
+            return
+
+        if not command.startswith("/"):
+            logging.error("Command does not start with /")
+            self.help(_from)
+            return
+
+        cmd = getattr(self, command[1:], None)
+
+        if not cmd:
+            self.help(_from)
+            return
+
+        try:
+            if len(value) > 0:
+                cmd(_from, *filter(lambda v: len(v) > 1, value.split(" ")))
+            else:
+                cmd(_from)
+        except (ValueError, TypeError):
+            logging.exception('Could not execute command %s' % command)
+            self.help(_from)
 
 settings = {
     "template_path": os.path.join(os.path.dirname(__file__), "templates"),
